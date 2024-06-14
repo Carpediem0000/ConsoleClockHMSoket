d9787ef86756b229fee647415c2d414f775de6f3
Ваня Зиновчук <zinovchukdfuz2@gmail.com>
2024-06-14 13:28:19 +0300

    Add project files.

---
+++ ConsoleClockHMSoketClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClockHMSoketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Loopback;
+            Socket socket = new Socket(iPAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(iPAddress, 56754));
+
+            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Waiting...");
+
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    int received = socket.ReceiveFrom(buffer, ref remoteEndPoint);
+                    string currentTime = Encoding.UTF8.GetString(buffer, 0, received);
+
+                    Console.Clear();
+                    Console.WriteLine($"Current time: {currentTime}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleClockHMSoketServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleClockHMSoketServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Loopback;
+
+            Socket udpSocket = new Socket(iPAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, 56754);
+
+
+            Console.WriteLine("Server start...");
+
+            while (true)
+            {
+                string currentTime = DateTime.Now.ToLongTimeString();
+                byte[] data = Encoding.UTF8.GetBytes(currentTime);
+
+                udpSocket.SendTo(data, endPoint);
+                Console.WriteLine($"Send time: {currentTime}");
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
